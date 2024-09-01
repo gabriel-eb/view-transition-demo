5c56d4c44b4f5661d93cb9f06a2c9fe3d5a5cf4b
geburguete <geburguete@gmail.com>
2024-09-01 06:14:05 +0000

    first impl for mpa transition

--- main.js
+++ main.js
@@ -1,62 +1,96 @@
-// Add JS here
+// Path where this app is deployed. Because we don’t deploy at the root of the domain
+// we need to keep track of this and adjust any URL matching using this value.
+const homePagePattern = new URLPattern(`/index.html`, window.origin);
+const isHomePage = (url) => {
+    return homePagePattern.exec(url);
+}
 
-document.addEventListener("keydown", (e) => {
-    console.log(e.code);
-    if (e.code === "ArrowRight") {
-        document.getElementById('next-page').click();
+const profilePagePattern = new URLPattern(`/profiles/:profile`, window.origin);
+const isProfilePage = (url) => {
+    return profilePagePattern.exec(url);
+}
+
+const extractProfileNameFromUrl = (url) => {
+    const match = profilePagePattern.exec(url);
+    return match?.pathname.groups.profile;
+}
+
+const setTemporaryViewTransitionNames = async (entries, vtPromise) => {
+    for (const [$el, name] of entries) {
+        $el.style.viewTransitionName = name;
     }
-});
 
-window.addEventListener("pageswap", async (e) => {
-    // Only run this if an active view transition exists
+    await vtPromise;
+
+    for (const [$el, name] of entries) {
+        $el.style.viewTransitionName = '';
+    }
+
+}
+
+// When going to a detail page, set `profile-name` and `profile-avatar` vt-names
+// on the elements that link to that detail page
+window.addEventListener('pageswap', async (e) => {
     if (e.viewTransition) {
-        const currentUrl = e.activation.from?.url
-            ? new URL(e.activation.from.url)
-            : null;
+        const currentUrl = e.activation.from?.url ? new URL(e.activation.from.url) : null;
         const targetUrl = new URL(e.activation.entry.url);
 
         // Going from profile page to homepage
         // ~> The big img and title are the ones!
         if (isProfilePage(currentUrl) && isHomePage(targetUrl)) {
-            // Set view-transition-name values on the elements to animate
-            document.querySelector(`#detail main h1`).style.viewTransitionName =
-                "name";
-            document.querySelector(`#detail main img`).style.viewTransitionName =
-                "avatar";
-
-            // Remove view-transition-names after snapshots have been taken
-            // Stops naming conflicts resulting from the page state persisting in BFCache
-            await e.viewTransition.finished;
-            document.querySelector(`#detail main h1`).style.viewTransitionName =
-                "none";
-            document.querySelector(`#detail main img`).style.viewTransitionName =
-                "none";
+            setTemporaryViewTransitionNames([
+                [document.querySelector(`#detail main h1`), 'animate-name'],
+                [document.querySelector(`#detail main img`), 'animate-avatar'],
+            ], e.viewTransition.finished);
         }
 
         // Going to profile page
         // ~> The clicked items are the ones!
         if (isProfilePage(targetUrl)) {
-            const profile = extractProfileNameFromUrl(targetUrl);
+            const profile = extractProfileNameFromUrl(targetUrl).replace(/\.[^/.]+$/, "");
 
-            // Set view-transition-name values on the elements to animate
-            document.querySelector(`#${profile} span`).style.viewTransitionName =
-                "name";
-            document.querySelector(`#${profile} img`).style.viewTransitionName =
-                "avatar";
+            const swap = document.querySelector(`#${profile} span`);
 
-            // Remove view-transition-names after snapshots have been taken
-            // Stops naming conflicts resulting from the page state persisting in BFCache
-            await e.viewTransition.finished;
-            document.querySelector(`#${profile} span`).style.viewTransitionName =
-                "none";
-            document.querySelector(`#${profile} img`).style.viewTransitionName =
-                "none";
+            setTemporaryViewTransitionNames([
+                [document.querySelector(`#${profile} span`), 'animate-name'],
+                [document.querySelector(`#${profile} img`), 'animate-avatar'],
+            ], e.viewTransition.finished);
         }
     }
 });
 
-const isProfilePage = (url) => url.pathname === "/profile";
+// When going from a detail page to the homepage, set `profile-name` and `profile-avatar` vt-names
+// on the list item for the profile that was viewed on the detail page.
+window.addEventListener('pagereveal', async (e) => {
 
-const isHomePage = (url) => url.pathname === "/";
+    if (!navigation.activation.from) return;
 
-const extractProfileNameFromUrl = (url) => url.pathname.split("/")[2];
+    if (e.viewTransition) {
+        const fromUrl = new URL(navigation.activation.from.url);
+        const currentUrl = new URL(navigation.activation.entry.url);
+        console.log(fromUrl);
+        console.log(currentUrl);
+
+        // Went from profile page to homepage
+        // ~> Set VT names on the relevant list item
+        if (isProfilePage(fromUrl) && isHomePage(currentUrl)) {
+            const profile = extractProfileNameFromUrl(fromUrl).replace(/\.[^/.]+$/, "");
+
+            const reveal = document.querySelector(`#${profile} span`);
+
+            setTemporaryViewTransitionNames([
+                [document.querySelector(`#${profile} span`), 'animate-name'],
+                [document.querySelector(`#${profile} img`), 'animate-avatar'],
+            ], e.viewTransition.ready);
+        }
+
+        // Went to profile page
+        // ~> Set VT names on the main title and image
+        if (isProfilePage(currentUrl)) {
+            setTemporaryViewTransitionNames([
+                [document.querySelector(`#detail main h1`), 'animate-name'],
+                [document.querySelector(`#detail main img`), 'animate-avatar'],
+            ], e.viewTransition.ready);
+        }
+    }
+});
