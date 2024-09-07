982f2e0f054310e8112c4cd1946d04786e05561f
geburguete <geburguete@gmail.com>
2024-09-07 02:12:09 +0000

    minor changes to format and back button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 // Add JS here
-
 document.addEventListener("keydown", (e) => {
   console.log(e.code);
   if (e.code === "ArrowRight") {
     document.getElementById('next-page').click();
+  }
+});
+document.addEventListener("keydown", (e) => {
+  console.log(e.code);
+  if (e.code === "ArrowLeft") {
+    document.getElementById('prev-page').click();
   }
 });
