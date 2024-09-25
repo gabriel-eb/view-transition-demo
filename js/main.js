b7f0b235f2894b5387c458e44490c77fbfc4c3cd
gabriel-eb <gabo_espburg@hotmail.com>
2024-09-25 18:06:37 +0000

    remove log

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 // Add JS here
 document.addEventListener("keydown", (e) => {
-  console.log(e.code);
   if (e.code === "ArrowRight") {
     document.getElementById('next-page').click();
   }
 });
 document.addEventListener("keydown", (e) => {
-  console.log(e.code);
   if (e.code === "ArrowLeft") {
     document.getElementById('prev-page').click();
   }
